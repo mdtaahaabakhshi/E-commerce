f94c6f276b3f85897753236ec1c5b81bd5e420e5
MD.Taahaa Bakhshi <taahaabakhshi7970@gamil.com>
2025-05-05 22:15:03 +0530

    product page done with related product fucntionality

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../context/shopContext'
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const LatestCollection = () => {
+
+    const { products} = useContext(ShopContext);
+    const [latestProducts,setLatestProducts]=useState([]);
+    useEffect(()=>{
+      setLatestProducts(products.slice(0,5));
+
+    },[]);
+
+  return (
+    <div className='my-10'>
+<div className='text-center py-8 text-3xl'>
+<Title text1={'LATEST'} text2={'COLLECTION'}/>
+<p className='w-3/4 m-auto text-base text-gray-600'>
+Lorem ipsum dolor sit amet consectetur, adipisicing elit. In labore illum consequuntur, blanditiis hic !</p>
+</div>
+
+{/* Rendering Products */}
+<div className='grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+    {
+        latestProducts.map((item,index)=>(
+<ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price}/>
+         ) )
+    }
+</div>
+    </div>
+  )
+}
+
+export default LatestCollection;
---
+++ frontend/src/components/ProductItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useContext } from 'react'
+import { ShopContext } from '../context/shopContext'
+import { Link } from 'react-router-dom';
+
+const ProductItem = ({id,image,name,price}) => {
+    const { currency } = useContext (ShopContext);
+  return (
+
+        <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
+        <div className='overflow-hidden'>
+            <img src={image[0]} alt={name} className='hover:scale-110 transition ease-in-out' />
+        </div>
+        <p className='pt-3 pb-1 text-sm'>{name}</p>
+        <p className='font-mb text-sm'>{currency}{price}</p>
+        </Link>
+  )
+}
+
+export default ProductItem;      
