6a0fdfa4247c59eb03b1a5aaa4878efb0146e414
MD.Taahaa Bakhshi <taahaabakhshi7970@gamil.com>
2025-05-09 23:39:41 +0530

    created product and user model

---
+++ backend/models/productModel.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: Array,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  subcategory: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  sizes: {
+    type: Array,
+    required: true,
+  },
+  bestsellers: {
+    type: Boolean,
+    // default: false,
+  },
+  date: {
+    type: Number,
+required :true
+},
+});
+
+const ProductModel = mongoose.models.product ||  mongoose.model("Product", productSchema);
+export default ProductModel;
---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({ 
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    // isAdmin: {
+    //     type: Boolean,
+    //     default: false,
+    // },
+    cartData:{
+        type: Object,
+        default: {},
+    },
+    }, {minimize: false, timestamps: true}
+);
+
+
+const UserModel = mongoose.models.user || mongoose.model("User", userSchema);
+export default UserModel;
