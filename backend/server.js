b12816a78ed9f70b507d73be32043acf5a1b78d9
MD.Taahaa Bakhshi <taahaabakhshi7970@gamil.com>
2025-05-09 23:15:42 +0530

    created server and configured moongose and cloudinary

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectToCloudinary from './config/cloudinary.js';
+
+//* App config
+const app = express();  
+const PORT = process.env.PORT || 5000;
+connectDB();
+connectToCloudinary();
+
+//* Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//* api endpoints
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
