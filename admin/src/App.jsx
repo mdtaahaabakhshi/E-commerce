055bef0d6f29732d05959bbf41a23825e559df98
MD.Taahaa Bakhshi <taahaabakhshi7970@gamil.com>
2025-05-12 23:01:27 +0530

    setuped project

---
+++ admin/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    
+    <div className='bg-gray-50 min-h-screen'>
+    <Navbar/>
+    <hr />
+    <div className='flex w-full'></div>
+    </div>
+  )
+}
+
+export default App
